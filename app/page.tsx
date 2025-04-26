1033198fa4f9ebd83dfbdb792803e26adde230a5
josue david kennedy <104701497+programfive@users.noreply.github.com>
2025-04-26 16:35:16 -0400

    starting project

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,59 @@
-import Image from "next/image";
+import Authors from "@/components/Authors/Authors";
+import LatestArticles from "@/components/LatestArticles/LatestArticles";
+import NewsLoading from "@/components/NewsTicker/loading";
+import LatestPodcasts from "@/components/LatestPodcasts/LatestPodcasts";
+import LatestPodcastsLoading from "@/components/LatestPodcasts/loading";
+import AuthorsLoading from "@/components/Authors/loading";
+import NewsTicker from "@/components/NewsTicker/NewsTicker";
+import PageTitle from "@/components/PageTitle";
+import Subheading from "@/components/Subheading";
+import { Suspense } from "react";
+
+export const metadata = {
+  title: "Fyrre Magazine | Art & Life | Home",
+  description: "Articles, podcasts and news from the Berlin cultural scene",
+};
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="flex flex-col min-h-screen max-w-[95rem] w-full mx-auto px-4 lg:pt-0 sm:pt-4 xs:pt-2 lg:pb-4 md:pb-4 sm:pb-2 xs:pb-2">
+      <PageTitle
+        className="sr-only"
+        imgSrc="/images/titles/Art&Life.svg"
+        imgAlt="The words 'Art & Life' in bold uppercase lettering"
+      >
+        Art & Life
+      </PageTitle>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <Suspense fallback={<NewsLoading />}>
+        <NewsTicker />
+      </Suspense>
+
+      <LatestArticles />
+
+      <Subheading
+        className="text-subheading"
+        url="/podcasts"
+        linkText="All episodes"
+      >
+        Podcast
+      </Subheading>
+
+      <Suspense fallback={<LatestPodcastsLoading />}>
+        <LatestPodcasts />
+      </Suspense>
+
+      <Subheading
+        className="text-subheading"
+        url="/authors"
+        linkText="All authors"
+      >
+        Authors
+      </Subheading>
+
+      <Suspense fallback={<AuthorsLoading />}>
+        <Authors />
+      </Suspense>
+    </main>
   );
 }
